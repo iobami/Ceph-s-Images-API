d350bbe14164098f6ad78335ffeb5090e3036c6b
iobami <aayobami@vasconsolutions.com>
2020-10-04 09:44:35 -0400

    batch upload setup

---
+++ utils.js
@@ -0,0 +1,23 @@
+const Task = require('./api/models/todoListModel');
+const db = require('./db/db');
+
+const imageUpload = (payload) => {
+  const new_task = new Task(payload);
+  return new_task.save();
+};
+
+const batchUpload = async () => {
+  for (const imageObject of db) {
+    try {
+      const response = await imageUpload(imageObject);
+      if (response) console.log('done');
+    } catch (error) {
+      console.log(error.message);
+      console.log(JSON.stringify(imageObject));
+    }
+  }
+};
+
+module.exports = {
+  batchUpload,
+};

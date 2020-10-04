50e3a746ce8a8cba2a9a88ed90ae023717ef44d8
iobami <aayobami@vasconsolutions.com>
2020-10-04 07:19:34 -0400

    clean up

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const port = process.env.PORT || 5000;
 
 app.listen(port);
 
-console.log('todo list RESTful API server started on: ' + port);
+console.log('RESTful API server started on: ' + port);
 
 // const toDo = require('./db/db.js');
 // console.log(toDo);
